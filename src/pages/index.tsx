354e4bdefad39e8ea16a0b2ef5624f9b5170aa33
Marton Lederer <marton@lederer.hu>
2021-12-21 12:43:25 +0100

    chore: landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,40 @@
+import { CodeIcon, DownloadIcon } from "@iconicicons/react";
+import Image from "next/image";
+import Button from "../components/Button";
+import styles from "../styles/views/Home.module.sass";
+
 export default function Home() {
   return (
-    <div>
-      hi
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-    </div>
+    <>
+      <div className={styles.Landing}>
+        <div className={styles.LandingContent}>
+          <div className={styles.LandingText}>
+            <h1>Smart Contracts.</h1>
+            <h2>
+              Write contracts in{" "}
+              <div className={styles.ContractLanguage}>
+                <span>Rust</span>
+                <div className={styles.Selector} />
+              </div>
+            </h2>
+            <p>
+              Deploy powerful contracts in a minute and make them permanent and
+              free for your users.
+            </p>
+            <div style={{ display: "flex" }}>
+              <Button secondary>
+                <DownloadIcon />
+                Download
+              </Button>
+              <Button>
+                <CodeIcon />
+                Read more
+              </Button>
+            </div>
+          </div>
+          <img src="/terminal.svg" draggable={false} />
+        </div>
+      </div>
+    </>
   );
 }
