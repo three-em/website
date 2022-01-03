01abcc6020f5780e71e1d1d017dbe13197bef913
Marton Lederer <marton@lederer.hu>
2022-01-03 18:57:23 +0100

    feat: rewrites

--- next.config.js
+++ next.config.js
@@ -3,5 +3,19 @@ const withImages = require("next-images");
 /** @type {import('next').NextConfig} */
 module.exports = {
   ...withImages(),
-  reactStrictMode: true
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/install.sh",
+        destination:
+          "https://raw.githubusercontent.com/three-em/3em/main/tools/release/install.sh"
+      },
+      {
+        source: "/install.ps1",
+        destination:
+          "https://raw.githubusercontent.com/three-em/3em/main/tools/release/install.ps1"
+      }
+    ];
+  }
 };
